4406101d5f90eb4d64ebb8f298a6922cc5af275e
hsneden <48335502+hcsneden@users.noreply.github.com>
2023-03-23 15:52:41 -0500

    main landing page

---
+++ src/components/Header.jsx
@@ -0,0 +1,49 @@
+import React from "react";
+import { Navbar, Container, Nav, NavDropdown, Button } from "react-bootstrap";
+import { useNavigate } from "react-router-dom";
+
+const Header = () => {
+
+
+    return (
+        <Navbar className="nav-bar" fixed="top">
+            <Button variant="primary">Contact us</Button>
+            <div>
+                <img
+                    alt=""
+                    src={require("../img/logo.png")}
+                    width='50%'
+                    className="logo"
+                />{" "}
+            </div>
+
+
+            
+            <div className="socials">
+            <a href="https://open.spotify.com/artist/242EQUEtpStM9vH0AoPwGt?si=suncu_mMTa61BoW2i9iCUg">
+                    <img
+                        alt="spotify logo"
+                        className="socials-img"
+                        src={require("../img/spotify.png")}
+                    />
+                </a>
+                <a href="https://www.instagram.com/_poorwill_">
+                    <img
+                        alt="instagram logo"
+                        className="socials-img"
+                        src={require("../img/instagram.png")}
+                    />
+                </a>
+                <a href="https://www.facebook.com/poorwillband">
+                    <img
+                        alt="facebook logo"
+                        className="socials-img"
+                        src={require("../img/fb.png")}
+                    />
+                </a>
+            </div>
+        </Navbar>
+    );
+};
+
+export default Header;
---
+++ src/components/Home.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+import { Button, Col, Row } from "react-bootstrap";
+
+const Home = () => {
+    return (
+        <div  className='main-content'>
+        <Row>
+            <Col lg={1} md={0} sm={0}></Col>
+            <Col>
+                <img className='band-photo'
+                    src={require("../img/EP.jpeg")} />
+            </Col>
+            <Col>
+                <p className="text-heading">Tough Kid EP out now</p>
+                <p>Available on all streaming platforms</p>
+                <Button variant="secondary" href="https://open.spotify.com/album/5bLA0l2kii9gD062sLMWt1?si=a3kmTCiqT9el8qvt8TrooQ">Listen Now</Button>
+            </Col>
+            <Col lg={1} md={0} sm={0}></Col>
+        </Row>
+
+            <div className="break"></div>
+
+            <div className='shows-container'>
+                <h1>Shows</h1>
+                <p className="show-item">Caydence Record Store, St. Paul MN</p>
+                <p>April 8, 2023 - 7PM</p>
+                <Button className="ticket-button">Tickets</Button>
+                <p className="show-item">Caydence Record Store, St. Paul MN</p>
+                <p>April 8, 2023 - 7PM</p>
+                <Button className="ticket-button" >Tickets</Button>
+            </div>
+
+
+        </div>
+    )
+}
+
+export default Home
